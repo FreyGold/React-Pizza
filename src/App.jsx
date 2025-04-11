c0f0c46734a36d819ebe7cdb42e66e9ab1a2971e
freygold <ahmedtawfik833@gmail.com>
2025-04-11 11:39:37 +0200

    finished the routing part, learned how to use actions and loaders

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,36 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
 import { RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import Order from "./features/order/Order";
-import CreateOrder from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
+import CreateOrder, {
+   action as createOrderAction,
+} from "./features/order/CreateOrder";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
    {
-      path: "/",
-      element: <Home />,
-   },
-   {
-      path: "/menu",
-      element: <Menu />,
-   },
-   {
-      path: "/cart",
-      element: <Cart />,
-   },
-   {
-      path: "/order/new",
-      element: <CreateOrder />,
-   },
-   {
-      path: "/order/:id",
-      element: <Order />,
+      element: <AppLayout />,
+      errorElement: <Error />,
+      children: [
+         { path: "/", element: <Home /> },
+
+         {
+            path: "/menu",
+            element: <Menu />,
+            loader: menuLoader,
+            errorElement: <Error />,
+         },
+         { path: "/cart", element: <Cart /> },
+         {
+            path: "/order/new",
+            element: <CreateOrder />,
+            action: createOrderAction,
+         },
+         { path: "/order/:orderId", element: <Order />, loader: orderLoader },
+      ],
    },
 ]);
 function App() {
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import { Form } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
+import { redirect } from "react-router-dom";
+import { useNavigation } from "react-router-dom";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -31,6 +35,8 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+   const navigation = useNavigation();
+   const isSubmitting = navigation.status === "submitting";
    // const [withPriority, setWithPriority] = useState(false);
    const cart = fakeCart;
 
@@ -38,7 +44,8 @@ function CreateOrder() {
       <div>
          <h2>Ready to order? Let's go!</h2>
 
-         <form>
+         {/* <Form method="POST" action="/order/new"> */}
+         <Form method="POST">
             <div>
                <label>First Name</label>
                <input type="text" name="customer" required />
@@ -72,11 +79,29 @@ function CreateOrder() {
             </div>
 
             <div>
-               <button>Order now</button>
+               <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+               <button disabled={isSubmitting}>
+                  {isSubmitting ? "Submitting order..." : "Order now"}
+               </button>
             </div>
-         </form>
+         </Form>
       </div>
    );
+}
+
+export async function action({ request }) {
+   const formData = await request.formData();
+
+   const data = Object.fromEntries(formData);
+
+   const order = {
+      ...data,
+      priority: data.priority === "on",
+      cart: JSON.parse(data.cart),
+   };
+
+   const newOrder = await createOrder(order);
+   return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
