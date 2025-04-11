e0bdad8a7cc983fcdae716db1e8b5180bd3520a3
freygold <ahmedtawfik833@gmail.com>
2025-04-11 12:43:59 +0200

    finished setting up tailwind css

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
-   return (
-      <header>
-         <Link to="/">Fast React Pizza Co.</Link>
-         <SearchOrder />
-      </header>
-   );
+  return (
+    <header className="flex text-center text-xl font-semibold text-yellow-500">
+      <Link to="/">Fast React Pizza Co.</Link>
+      <SearchOrder />
+    </header>
+  );
 }
 
 export default Header;
