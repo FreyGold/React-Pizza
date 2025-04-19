c413d6e77a6be9340b6d191ee27b947d98cb0a7c
freygold <ahmedtawfik833@gmail.com>
2025-04-19 23:51:40 +0200

    d

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,37 @@ import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order, { loader as orderLoader } from "./features/order/Order";
 import CreateOrder, {
-   action as createOrderAction,
+  action as createOrderAction,
 } from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
 
 const router = createBrowserRouter([
-   {
-      element: <AppLayout />,
-      errorElement: <Error />,
-      children: [
-         { path: "/", element: <Home /> },
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <Home /> },
 
-         {
-            path: "/menu",
-            element: <Menu />,
-            loader: menuLoader,
-            errorElement: <Error />,
-         },
-         { path: "/cart", element: <Cart /> },
-         {
-            path: "/order/new",
-            element: <CreateOrder />,
-            action: createOrderAction,
-         },
-         { path: "/order/:orderId", element: <Order />, loader: orderLoader },
-      ],
-   },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      { path: "/cart", element: <Cart /> },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
+      { path: "/order/:orderId", element: <Order />, loader: orderLoader },
+    ],
+    basename: "/React-Pizza",
+  },
 ]);
 function App() {
-   return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
